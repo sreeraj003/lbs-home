4ccb774bc24343b2c931d71099c5f8c6dec809b4
Sreeraj <sreeraj@scita.in>
2025-12-19 11:16:49 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Announcements from "./pages/announcements/Announcements"
+import Announcements from "./pages/announcements/Announcements.jsx"
 import Event from "./pages/events/Event.jsx"
-import Contact from "./pages/contact/contact"
-import Membership from "./pages/membership/Membership"
-import Layout from "./layout/Layout"
-import Home from "./pages/home/Home"
+import Contact from "./pages/contact/Contact.jsx"
+import Membership from "./pages/membership/Membership.jsx"
+import Layout from "./layout/Layout.jsx"
+import Home from "./pages/home/Home.jsx"
 
 
 function App() {
